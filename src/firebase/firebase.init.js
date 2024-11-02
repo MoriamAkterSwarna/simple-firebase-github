212484be4622f77426848af118ddbc1deb7a8a46
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2024-11-02 22:52:04 +0600

    Github Email is not showing issue resolved

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBob0KXQlOgcKfCuzMeqvxO7ofsscL8zbQ",
+  authDomain: "test-3ff21.firebaseapp.com",
+  projectId: "test-3ff21",
+  storageBucket: "test-3ff21.firebasestorage.app",
+  messagingSenderId: "804711583453",
+  appId: "1:804711583453:web:6a5b5f860f596eebcdd534"
+};
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
